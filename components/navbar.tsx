ec545cdac4296c1104bae261ae0a19237a4aeeeb
mgu83 <gumaggie333@gmail.com>
2023-01-04 22:38:58 -0800

    getting ready to upload

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import logo from "../public/logo.svg";
 import { Logo } from "../components/logo";
+import Link from "next/link";
 
 function NavLink({ to, children }) {
   return (
@@ -19,9 +20,9 @@ function MobileNav({ open, setOpen }) {
       <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20">
         {" "}
         {/*logo container*/}
-        <a className="text-xl font-semibold" href="/">
-          <img src={logo} alt="MAGGIE GU" />
-        </a>
+        <Link href="/" className="text-2xl font-semibold">
+          home
+        </Link>
       </div>
       <div className="flex flex-col ml-4">
         <a
@@ -57,10 +58,9 @@ export default function Navbar() {
     <nav className="flex filter drop-shadow-md bg-stone-50 px-4 py-4 h-20 items-center">
       <MobileNav open={open} setOpen={setOpen} />
       <div className="w-3/12 flex items-center">
-        <a className="text-2xl font-semibold" href="/">
-          {/* <img src={logo} alt="MAGGIE GU" /> */}
+        <Link href="/" className="text-2xl font-semibold">
           home
-        </a>
+        </Link>
       </div>
       <div className="w-9/12 flex justify-end items-center">
         <div
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    unoptimized: true
+  }
 }
 const withImages = require('next-images')
 module.exports = nextConfig, withImages
